3edad626216b8c97799ec52e44e83ddd560f45dd
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2016-03-03 11:01:43 -0300

    Update footer.php

--- src/templates/master/footer.php
+++ src/templates/master/footer.php
@@ -1,8 +1,8 @@
 
-	{{ Html::script()->src( App::getWWWDir("node_modules/jquery/jquery.min.js") ) }}
-	{{ Html::script()->src( App::getWWWDir("node_modules/angular/angular.min.js") ) }}
-	{{ Html::script()->src( App::getWWWDir("jscript/angular/app.js") ) }}
-	{{ Html::script()->src( App::getWWWDir("jscript/angular/controller.js") ) }}
-	{{ Html::script()->src( App::getWWWDir("jscript/angular/service.js") ) }}
-	{{ Html::script()->src( App::getWWWDir("jscript/angular/filter.js") ) }}
-	{{ Html::script()->src( App::getWWWDir("jscript/angular/directive.js") ) }}
+	{{ Html::script()->src( Dir::getWWW("node_modules/jquery/jquery.min.js") ) }}
+	{{ Html::script()->src( Dir::getWWW("node_modules/angular/angular.min.js") ) }}
+	{{ Html::script()->src( Dir::getWWW("jscript/angular/app.js") ) }}
+	{{ Html::script()->src( Dir::getWWW("jscript/angular/controller.js") ) }}
+	{{ Html::script()->src( Dir::getWWW("jscript/angular/service.js") ) }}
+	{{ Html::script()->src( Dir::getWWW("jscript/angular/filter.js") ) }}
+	{{ Html::script()->src( Dir::getWWW("jscript/angular/directive.js") ) }}
