1eae30db364bf1d837dd06ada58568dde8a50c0f
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2017-08-16 11:41:31 -0300

    removendo view de erro do composer

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
 
-if( file_exists('vendor/autoload.php') )
-{
-	require_once 'vendor/autoload.php';
-}
-else
-{
-	require_once 'src/views/error/error-composer.php';
-}
+require_once 'vendor/autoload.php';
